fc31e2f4e1c68ce34e6ec3f34004b591faddee50
Komissarov Semyon / amerlon- <komissarov.semyon@gmail.com>
2019-05-16 15:17:52 +0300

    added GET:/movie_schedule

--- backend/app.py
+++ backend/app.py
@@ -46,5 +46,42 @@ def get_current_movies(city_id, date_str=None):
 
     return jsonify(result)
 
+
+@app.route('/api/movie_schedule/<city_id>/<movie_id>')
+@app.route('/api/movie_schedule/<city_id>/<movie_id>/<date_str>')
+def get_movie_schedule(city_id, movie_id, date_str=None):
+    if date_str is None:
+        date_str = datetime.today().strftime('%Y-%m-%d')
+
+    cur = conn.cursor()
+    columns = ('id', 'ya_id', 'date', 'price_min', 'price_max', 'cinema_name', 'cinema_address')
+    cur.execute("""
+    SELECT
+        s.session_id,
+        s.ya_id,
+        s.date,
+        s.price_min,
+        s.price_max,
+        c.name,
+        c.address
+    FROM sessions s
+    JOIN cinemas c on s.cinema_id = c.cinema_id
+    WHERE DATE(s.date) = %(date)s AND
+      s.movie_id = %(movie_id)s AND
+      c.city_id = %(city_id)s
+    """, {
+        'date': date_str,
+        'movie_id': movie_id,
+        'city_id': city_id
+    })
+
+    result = {
+        'sessions': [dict(zip(columns, i)) for i in cur.fetchall()],
+        'date': date_str
+    }
+
+    return jsonify(result)
+
+
 if __name__ == '__main__':
     app.run()
